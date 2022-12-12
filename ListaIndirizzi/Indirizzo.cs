9a06124cc0809eecd2522830fd1f037c7f6b0076
GioPa97 <giovannipagliara97@.com>
2022-12-12 15:30:15 +0100

    creato programma principale

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,80 @@
+﻿namespace ListaIndirizzi;
+
+public class Indirizzo
+{
+    string nome;
+    string cognome;
+    string via;
+    string città;
+    string provincia;
+    string zip;
+
+    public Indirizzo(string nome, string cognome, string via, string città, string provincia, string zip)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.via = via;
+        this.città = città;
+        this.provincia = provincia;
+        this.zip = zip;
+    }
+    public string getNome()
+    {
+        return nome;
+    }
+    public string getCognome()
+    {
+        return cognome;
+    }
+
+    public string getVia()
+    {
+        return città;
+    }
+
+    public string getCittà()
+    {
+        return città;
+    }
+
+    public string getProvincia()
+    {
+        return città;
+    }
+
+    public string getZip()
+    {
+        return città;
+    }
+
+    public void setNome(string nome)
+    {
+        nome = this.nome;
+    }
+
+    public void setCognome(string cognome)
+    {
+        cognome = this.cognome;
+    }
+
+    public void setVia(string via)
+    {
+        via = this.via;
+    }
+
+    public void setCittà(string città)
+    {
+        città = this.città;
+    }
+
+    public void setProvincia(string provincia)
+    {
+        provincia = this.provincia;
+    }
+
+    public void setZip(string zip)
+    {
+        zip = this.zip;
+    }
+
+};
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using ListaIndirizzi;
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\Utente\\Contacts\\Desktop\\Esercizi Github\\csharp-lista-indirizzi\\ListaIndirizzi\\ListaIndirizzi.csproj");
+
+    while (!file.EndOfStream)
+    {
+
+#pragma warning disable CS8600 // Conversione del valore letterale Null o di un possibile valore Null in un tipo che non ammette i valori Null.
+        string riga = file.ReadLine();
+#pragma warning restore CS8600 // Conversione del valore letterale Null o di un possibile valore Null in un tipo che non ammette i valori Null.
+
+
+        string[] informazioniIndirizzo = riga.Split(" ,");
+        string nome = informazioniIndirizzo[0];
+        string cognome = informazioniIndirizzo[1];
+        string via = informazioniIndirizzo[2];
+        string città = informazioniIndirizzo[3];
+        string provincia = informazioniIndirizzo[4];
+        string zip = informazioniIndirizzo[5];
+
+
+        Indirizzo indirizzoEstratto = new Indirizzo(nome, cognome, via, città, provincia, zip);
+        listaIndirizzi.Add(indirizzoEstratto);
+        // Console.WriteLine(riga);
+    }
+
+    file.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+foreach (Indirizzo indirizzo in listaIndirizzi)
+{
+    Console.WriteLine(indirizzo);
+}
